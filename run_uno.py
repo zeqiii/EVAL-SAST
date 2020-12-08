e54c8ad34d79e721bdb5502081976ed221ad08ac
zeqihao <zeqihao@gmail.com>
2020-12-08 16:59:12 +0800

    Create run_uno.py

---
+++ run_uno.py
@@ -0,0 +1,44 @@
+# -*- coding=utf-8 -*-
+import os, sys, csv
+from impl import *
+from glo import *
+from bug import *
+
+class Runner_uno(Runner):
+    def __init__(self):
+        Runner.__init__(self)
+        self.tool = "uno"
+
+    def _genCMD(self, testcase, output_path, output_file="result.out"):
+        if not os.path.exists(testcase.testcase_dir_abs):
+            raise Exception(testcase.testcase_dir_abs + " does not exist")
+        build_command = ""
+        # 根据不同的测试集，实现不同的编译方法
+        # uno
+        if not os.path.exists(output_path):
+            os.makedirs(output_path) # 对于splint，需要我们来创建存放检测结果的文件夹
+        # 检测结果存放在result.xml文件中
+        cmd = "uno %s/*.c > %s" %(testcase.testcase_dir_abs, os.path.join(output_path, output_path))
+        return cmd
+
+    def _parseOutput(self, testcase, output_path, output_file="result.out"):
+        bugs = []
+        with open(os.path.join(output_path, output_file)) as fp:
+            line = fp.readline()
+            while line:
+                if line.find("check completed"):
+                    break
+                if line.find("uno:") > 0:
+                    bug = Bug()
+                    bug.testcase_id = testcase.testcase_id
+                    line2 = fp.readline()
+                    line3 = fp.readline()
+                    line = line.strip().lstrip("uno:").strip()
+                    bug.description = line.split(":")[2].strip()
+                    bug.sink.file = line.split(":")[0].split(testcase.testcase_dir)[-1].strip("/")
+                    bug.sink.line = int(line.split(":")[1].strip())
+                    bugs.append(bug)
+        return bugs
+
+if __name__ == "__main__":
+    pass
