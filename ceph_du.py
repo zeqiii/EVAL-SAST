74b3f04f38a04c081a8bdcd595f084f0e3390107
zeqiii <zeqiii@163.com>
2020-12-14 16:07:04 +0800

    fix ceph

--- ceph_du.py
+++ ceph_du.py
@@ -6,7 +6,7 @@ op = com_fm("ceph2", "client.vkg")
 parser = argparse.ArgumentParser(description="ceph upload download")
 parser.add_argument("--c", type=int, required=True, help="0 upload, 1 download")
 parser.add_argument("--r", type=str, required=True, help="upload remote path")
-parser.add_argument("--l", type=int, required=True, help="download local path")
+parser.add_argument("--l", type=str, required=True, help="download local path")
 
 def upload(local, remote):
 	res = op.upload('vkg', local, remote)
@@ -19,6 +19,6 @@ def download(remote, local):
 if __name__ == "__main__":
 	args = parser.parse_args()
 	if args.c == 0:
-		upload(args.l, args,r)
+		upload(args.l, args.r)
 	else:
 		download(args.r, args.l)
