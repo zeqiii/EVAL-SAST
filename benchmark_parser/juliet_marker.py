33b187bd3258712ba5fc5c4e5a7f81622a2d1896
zeqihao <zeqihao@gmail.com>
2021-01-06 20:01:24 +0800

    Update juliet_marker.py

--- benchmark_parser/juliet_marker.py
+++ benchmark_parser/juliet_marker.py
@@ -2,10 +2,20 @@
 import os, sys, argparse
 sys.path.append("..")
 from glo import *
-import juliet_parser
 
-tool = os.path.join(Config.LIBS, "tooling_sample")
-f_fun_info = os.path.join(Config.TMP, "func_line.info")
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass 
+    return False
 
 # 从Juliet的文件名中提取测试样本名，如CWE114_Process_Control__w32_char_connect_socket_07.c 提取 CWE114_Process_Control__w32_char_connect_socket
 def getSignature(filename):
@@ -20,40 +30,6 @@ def getSignature(filename):
             break
     signature = filename.split("_"+num)[0]
     return signature
-
-# gen function line info using libtooling
-def gen_func_info(sourcefile):
-    # -DINCLUDEMAIN 是为了适配juliet的编译而增加的预定义宏
-    cmd = "%s %s -extra-arg=\"-DINCLUDEMAIN -I%s\" > %s 2>/dev/null"%(tool, sourcefile, os.path.dirname(sourcefile), f_fun_info)
-    os.system(cmd)
-    f = open(f_fun_info)
-    lines = f.readlines()
-    f.close()
-    return lines
-
-def parse_func_info(lines, filters=[]):
-    # {
-    #  filename: 
-    #  {fun_name1: (startline, endline), fun_name2: (startline, endline)}
-    # }
-    info = {}
-    for line in lines:
-        funname = line.split("#")[0]
-        funinfo = line.split("#")[1]
-        if funinfo.find("/usr") > 0:
-            continue
-        for one in filters:
-            if funinfo.find(one) < 0:
-                continue
-        funinfo = funinfo.split(":")
-        filepath = funinfo[1]
-        if filepath not in info.keys():
-            info[filepath] = {}
-        if funname not in info[filepath].keys():
-            info[filepath][funname] = {}
-        linenum = funinfo[2]
-        info[filepath][funname][funinfo[0]] = linenum
-    return info
 
 # 用一种简单暴力的方式标注目标程序中的反例所在位置
 # keywords = {signature: keyword}
@@ -71,7 +47,7 @@ def mark_counterexamples(in_dir, keywords):
 
             with open(ff, "r") as fp:
                 content = fp.readlines()
-                
+
             keyword = keywords[signature]
             for i in range(0, len(content)):
                 if content[i].find(keyword) >= 0 and content[i].find("##bug##") < 0 \
