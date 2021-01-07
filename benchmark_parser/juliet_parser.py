4441c5662a190aef6c622941d3c1c45bd8e0f486
zeqihao <zeqihao@gmail.com>
2021-01-07 11:14:14 +0800

    Update juliet_parser.py

--- benchmark_parser/juliet_parser.py
+++ benchmark_parser/juliet_parser.py
@@ -114,7 +114,6 @@ def create_single_testcase(juliet_home_dir, outdir, cwe_list=[], preprocessed_bu
             relative_path = os.path.join(sig.split("__")[0], sig)
             outpath = os.path.join(outdir, relative_path)
             testcase.testcase_dir = relative_path
-            testcase.testcase_type = 
             testcase.testcase_dir_abs = os.path.abspath(outpath)
             testcase.testcase_id = sig
             if sig.endswith("_01"):
